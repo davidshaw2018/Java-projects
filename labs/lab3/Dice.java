cd43a4595ce578d87fa751ac9cebb9b92ad65a77
davidshaw <davidshaw@davids-mbp-2.dhcp.wustl.edu>
2017-02-06 23:53:06 -0600

    df

---
+++ labs/lab3/Dice.java
@@ -0,0 +1,68 @@
+package lab3;
+
+import java.util.Arrays;
+
+import cse131.ArgsProcessor;
+
+public class Dice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int numDice = ap.nextInt("How many dice are we using?");
+		int numThrows = ap.nextInt("How many throws will you make?");
+		int[] diceThrown = new int[numDice];
+		int j = 0;
+		int numSame = 0;
+		int[] diceSum = new int[5 * numDice];
+		
+
+		while (j < numThrows) {
+			int firstRoll=-1;
+			boolean allSame = true;
+			int sum = 0;
+			for (int i = 0; i < numDice; ++i) {
+				int v = 0;
+				if ((6 * Math.random()) < 1) {v = 1;}
+				else if ((6 * Math.random()) < 2) {v = 2;}
+				else if ((6 * Math.random()) < 3) {v = 3;} 
+				else if ((6 * Math.random()) < 4) {v = 4;}
+				else if ((6 * Math.random()) < 5) {v = 5;}
+				else {v = 6;}
+				diceThrown[i] = v;
+				if(i == 0){
+					firstRoll = v;
+				}
+				else{
+					if(v != firstRoll){
+						allSame = false;
+					}
+				}
+				sum = sum + diceThrown[i];
+				System.out.print(diceThrown[i] + "    ");
+				
+				
+				
+			}
+			System.out.print(sum + " ");
+			
+			if(allSame){
+				numSame++;
+				System.out.print("all same");
+			}
+			j = j + 1;
+			System.out.println("");
+			for (int k = numDice; k <= 6 * numDice; ++k) {
+				if (k == sum) {
+				diceSum[k - numDice] = diceSum[k - numDice] + 1;
+			}
+
+
+		} 	
+		double percentSame = 100 * (double)(numSame) / (double)numThrows;
+		System.out.println("The number of identical rolls is " + numSame);
+		System.out.println("The percentage of identical rolls is " + percentSame);
+
+	}
+}
+
