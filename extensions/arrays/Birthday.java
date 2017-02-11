4462bd0080cc8efeef8724f1b9c52bbbe6f6d130
davidshaw <davidshaw@davids-mbp-2.dhcp.wustl.edu>
2017-02-11 17:40:20 -0600

    bday formatting

--- extensions/arrays/Birthday.java
+++ extensions/arrays/Birthday.java
@@ -17,31 +17,61 @@ public class Birthday {
 		int sameBirthdays = 0;
 		int[] monthBirthdays = new int[12];
 		int[] dayBirthdays = new int[31];
+		System.out.println("Calendar:");
+		System.out.println();
 		for (int m = 0; m < 12; ++m) {
 			for (int d = 0; d < 31; ++d) {
 				if (calendar[m][d] > 1) {
-					sameBirthdays = sameBirthdays + 1;
+					sameBirthdays = sameBirthdays + calendar[m][d];
 				}
 				monthBirthdays[m] = monthBirthdays[m] + calendar[m][d];
 				dayBirthdays[d] = dayBirthdays[d] + calendar[m][d];	
+				System.out.print(calendar[m][d] + " ");
 			}
-
+				System.out.println();
+		}
+		System.out.println();
+		System.out.println("Birthdays per month: ");
+		for (int m = 0; m < 12; ++m) {
+			System.out.print(monthBirthdays[m] + " ");
+		}
+		System.out.println();
+		System.out.println();
+		System.out.println("Birthdays by day: ");
+		for (int d = 0; d < 31; ++d) {
+			System.out.print(dayBirthdays[d] + " ");
 		}
 		double[] monthPercent = new double[12];
 		double[] dayPercent = new double[31];
 		double monthSum = 0;
 		double daySum = 0;
-
+		System.out.println();
+		System.out.println();
+		System.out.print("Percent of birthdays by month: ");
+		System.out.println();
+		
 		for (int i = 0; i < monthPercent.length; ++i) {
 			monthPercent[i] = (double)Math.round(1000 * (double)monthBirthdays[i] / N) / 10;
 			monthSum = monthSum + monthPercent[i];
+			System.out.print(monthPercent[i] + "%  ");
 		}
+		System.out.println();
+		System.out.println();
+		System.out.println("Percent of birthdays by day: ");
+		
 		for (int i = 0; i < dayPercent.length; ++i) {
 			dayPercent[i] = (double)Math.round(1000 * (double)dayBirthdays[i] / N) / 10;
 			daySum = (double)Math.round(10 * (daySum + dayPercent[i])) / 10;
+			System.out.print(dayPercent[i] + "%  ");
 		}
-		System.out.println(sameBirthdays);
-
+		System.out.println();
+		double monthAverage = (double)Math.round(10 * monthSum / 12) / 10;
+		double dayAverage = (double)Math.round(10 * daySum / 31) / 10;
+		double samePercent = (double)Math.round(10 * (double)sameBirthdays / N) / 10;
+		System.out.println();
+		System.out.println("Monthly percent average: " + monthAverage + "%");
+		System.out.println("Daily percent average: " + dayAverage + "%");
+		System.out.println("Percent of people with same birthday: " + samePercent + "%");
 	}
 
 }
